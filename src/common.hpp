dc44f0ae7e64df78cff43e523f58cb7fa16235a2
William Narchi <willy.narchi@gmail.com>
2022-10-10 22:52:23 +0200

    Remove unused Pixel8bpc struct

--- src/common.hpp
+++ src/common.hpp
@@ -3,36 +3,6 @@
 
 #include <stdint.h>
 
-struct Pixel8bpc {
-    uint8_t r, g, b;
-
-    // Pixel8bpc(uint8_t value) : r(value), g(value), b(value) {};
-
-    friend bool operator==(const Pixel8bpc& lhs, const Pixel8bpc& rhs) {
-        return (lhs.r == rhs.r) && (lhs.g == rhs.g) && (lhs.b == rhs.b);
-    }
-
-    friend bool operator!=(const Pixel8bpc& lhs, const Pixel8bpc& rhs) {
-        return !(lhs == rhs);
-    }
-
-    friend Pixel8bpc operator*(Pixel8bpc lhs, const Pixel8bpc& rhs) {
-        return {lhs.r * rhs.r, lhs.g * rhs.g, lhs.b * rhs.b};
-    }
-
-    template<typename T>
-    friend Pixel8bpc operator*(Pixel8bpc lhs, const T& rhs) {
-        return {lhs.r * rhs, lhs.g * rhs, lhs.b * rhs};
-    }
-
-    Pixel8bpc& operator+=(const Pixel8bpc& rhs) {
-        this->r += rhs.r;
-        this->g += rhs.g;
-        this->b += rhs.b;
-        return *this;
-    }
-};
-
 template<typename T>
 bool threeOrMoreIdentical(T a, T b, T c, T d) {
     return ((a == b && b == c) ||
